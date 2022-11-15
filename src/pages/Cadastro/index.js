bc366d3b7a4e7c1ad543e6b3259696cd26c572a2
jlmsdev <joaosouza1135@gmail.com>
2022-11-15 18:43:32 -0300

    Tela de Login Concluida Visual

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,10 +1,60 @@
 import './cadastro.css';
 
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Cadastro() {
+    const [inputEmail, setInputEmail] = useState('');
+    const [inputSenha, setInputSenha] = useState('');
+
+    function cadastrar(e) {
+        e.preventDefault();
+
+        if(inputEmail === '' || inputSenha === '') {
+
+            toast.warn('Preencha todos os Campos', {
+                theme:'dark',
+                position: 'bottom-center'
+            })
+        }
+
+    }
+
+
+
     return(
-        <div>
-            Register
+        <div className='homeContainer'>
+            <h1>Crie Sua Conta</h1>
+            <p>Comece Agora a Gerenciar suas Tarefas</p>
+
+            <form className='form' onSubmit={cadastrar}>
+
+                <label className='title'>E-mail</label>
+                <input 
+                    type="email" 
+                    placeholder='Digite seu E-mail'
+                    value={inputEmail}
+                    onChange={ (e) => setInputEmail(e.target.value) }
+                />
+
+                <label className='title'>Crie uma Senha</label>
+                <input 
+                    type="password" 
+                    placeholder='*******'
+                    value={inputSenha}
+                    onChange={ (e) => setInputSenha(e.target.value) }
+                />
+
+                <button className='btnAcessar' type='submit'>
+                    Acessar
+                </button>
+
+            </form>
+
+            <Link className='linkCadastro' to='/'>
+                Já Possui uma Conta ? Faça seu Login
+            </Link>
         </div>
-    )
+    );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,62 @@
 import './home.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function Home() {
+    const [inputEmail, setInputEmail] = useState('');
+    const [inputSenha, setInputSenha] = useState('');
+
+    function logarApp(e) {
+        e.preventDefault();
+
+        if(inputEmail === '' || inputSenha === '') {
+
+            toast.warn('Preencha todos os Campos', {
+                theme:'dark',
+                position: 'bottom-center'
+            })
+        }
+
+        
+        
+        
+    }
+
+
+
     return(
-        <div>
-            Pagina Home
+        <div className='homeContainer'>
+            <h1>Lista de Tarefas App</h1>
+            <p>Gerencie suas Tarefas de Forma Simples.</p>
+
+            <form className='form' onSubmit={logarApp}>
+
+                <label className='title'>E-mail</label>
+                <input 
+                    type="email" 
+                    placeholder='Digite seu E-mail'
+                    value={inputEmail}
+                    onChange={ (e) => setInputEmail(e.target.value) }
+                />
+
+                <label className='title'>Senha</label>
+                <input 
+                    type="password" 
+                    placeholder='*******'
+                    value={inputSenha}
+                    onChange={ (e) => setInputSenha(e.target.value) }
+                />
+
+                <button className='btnAcessar' type='submit'>
+                    Acessar
+                </button>
+
+            </form>
+
+            <Link className='linkCadastro' to='/cadastro'>
+                Não Possui uma Conta? Cadastre-se!
+            </Link>
         </div>
     );
 }
